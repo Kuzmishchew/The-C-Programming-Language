13002547262ea237325bbefa0345da357a8a4d1a
Kashmir <kuzmishchew@gmail.com>
2023-11-25 00:06:06 +0500

    feat: done ex 1-17

---
+++ Chapter 1/Exercise 1-17.c
@@ -0,0 +1,49 @@
+/*
+Exercise:
+
+Write a program to print all input lines that are longer than 80 characters.
+*/
+
+#include <stdio.h>
+
+#define MAXLENGTH	1000
+
+void printLine(char line[]);
+
+int main() 
+{
+	int c, i, clength;
+	char line[MAXLENGTH];
+	
+	for (i = 0; i < MAXLENGTH; ++i)
+		line[i] = 0;	
+	
+	i = 0;
+	while((c = getchar())!= EOF) 
+		if (c != '\n') {
+			line[i] = c;
+			++i;
+		} else if (i > 80) {
+			printLine(line);
+			for (i = 0; i < MAXLENGTH; ++i)
+				line[i] = 0;
+			i = 0;
+		} else {
+			for (i = 0; i < MAXLENGTH; ++i)
+				line[i] = 0;
+			i = 0;
+		}	
+}
+
+void printLine(char line[])
+{
+	int i;
+	
+	i = 0;
+	while (line[i] != '\0') {
+		putchar(line[i]);
+		++i;
+	}
+	
+	printf("\n");
+}
